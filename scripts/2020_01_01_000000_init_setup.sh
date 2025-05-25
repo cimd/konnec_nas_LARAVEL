5e7abec6c0b9fa236b3376cb3c257ac4e776559a
Christian D'Aquino <christian.daquino@gmail.com>
2025-05-25 15:59:57 +0800

    d

--- scripts/2020_01_01_000000_init_setup.sh
+++ scripts/2020_01_01_000000_init_setup.sh
@@ -7,4 +7,5 @@ sudo apt -y upgrade
 sudo apt install curl ca-certificates apt-transport-https software-properties-common \
 gnupg lsb-release unzip smbclient \
 git -y
+sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
